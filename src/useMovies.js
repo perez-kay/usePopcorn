bbd813d805e6f08221ae1faf91c13987758c8321
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-12 12:27:11 -0700

    removed callback from custom hook

--- src/useMovies.js
+++ src/useMovies.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 
 const KEY = '277c1bff';
 
-export function useMovies(query, callback) {
+export function useMovies(query) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   useEffect(
     function () {
-      callback?.();
+      //   callback?.();
       const controller = new AbortController();
       async function fetchMovies() {
         try {
@@ -41,7 +41,7 @@ export function useMovies(query, callback) {
         controller.abort();
       };
     },
-    [query, callback]
+    [query]
   );
 
   return { movies, isLoading, error };
